ce55814afe0e780333c7fb13a9798f298632b75d
Irena Chen <irena1009@gmail.com>
2021-10-24 21:40:52 -0700

    prevent the pic from being dragged - js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+const slider = document.querySelector('.slider-container'), //QuerySelector return NodeList
+      slides = Array.from(document.querySelectorAll('.slide'))
+//take an array like object and turn it into array
+
+let isDragging = false,
+    startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID = 0, //request animation frame
+    currentIndex = 0
+
+//Prevent the pic from being dragged
+slides.forEach((slide, index) => {
+    const slideImage = slide.querySelector('img')
+    slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+})
 
