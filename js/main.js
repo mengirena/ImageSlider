874334a96bc6bc6c1d7a6a6724d1dad747a46bb3
Irena Chen <irena1009@gmail.com>
2021-10-25 21:59:42 -0700

    create functions to response to mouse and touch reation

--- js/main.js
+++ js/main.js
@@ -9,9 +9,38 @@ let isDragging = false,
     animationID = 0, //request animation frame
     currentIndex = 0
 
-//Prevent the pic from being dragged
+
 slides.forEach((slide, index) => {
     const slideImage = slide.querySelector('img')
+
+    //Prevent the pic from being dragged
     slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+
+    //Touch events
+    slide.addEventListener('touchstart', touchStart(index))
+    slide.addEventListener('touchend', touchEnd)
+    slide.addEventListener('touchmove', touchMove)
+    
+    //Mouse events
+    slide.addEventListener('mousedown', touchStart(index))
+    slide.addEventListener('mouseup', touchEnd)
+    slide.addEventListener('mouseleave', touchEnd)
+    slide.addEventListener('mousemove', touchMove)
+    
 })
 
+function touchStart(index){
+    return function(event){
+        isDragging = true
+    }
+}
+
+function touchEnd(){
+    isDragging = false
+}
+
+function touchMove(){
+    if (isDragging){
+        console.log("move")
+    }
+}
